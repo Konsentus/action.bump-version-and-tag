ecbf36c45b8101579afa287f43663ebf2be65f25
odellt <tom.odell@konsentus.com>
2019-11-20 15:45:07 +0000

    :construction: Add previous version tag as output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,51 +1,59 @@
 #!/bin/bash -l
 
+# Configure git cli tool
 git config --global user.email "actions@github.com"
 git config --global user.name "${GITHUB_ACTOR}"
 
+# Retrieve current branch name
 branch_name=${GITHUB_REF##*/}
 
+# Create tag prefix
 version_tag_prefix=${branch_name}/v
-echo "version_tag_prefix:${version_tag_prefix}"
 
-# previous_version_tag=$(git describe --abbrev=0 --match="${version_tag_prefix}*")
-
+# Get last annotated tag from previous commits, matching the tag prefix pattern
 previous_version_tag=$(git describe --abbrev=0 --match="${version_tag_prefix}*")
 
-if [ -z "${previous_version_tag}" ]; then
+if [ $? -ne 0 ] || [ -z "${previous_version_tag}" ]; then
+  # No previous tag found, so start version at 0.0.0
   echo "Failed to find any previous version tags with the prefix ${version_tag_prefix}. Initial version will be 0.0.0"
   new_version=0.0.0
 else
-  echo "previous_version_tag:${previous_version_tag}"
+  echo "::set-output name=previous_version_tag::${previous_version_tag}"
 
-  previous_semantic_version=${previous_version_tag##*/v}
-  echo "previous_semantic_version:${previous_semantic_version}"
+  # Strip prefix from the previous version tag
+  previous_semantic_version=${previous_version_tag#${version_tag_prefix}}
 
+  # Gather commit message from all commits since the last version tag to now
   commit_messages_since_last_version=$(git log --pretty=oneline HEAD...${previous_version_tag})
-  echo "commit_messages_since_last_version:${commit_messages_since_last_version}"
 
+  # Default version bump level is patch
   version_bump_level=patch
 
+  # Search commit messages for presence of #major or #minor to determine verison bump level
+  # #major takes precedence
   if [[ $commit_messages_since_last_version == *"#major"* ]]; then
     version_bump_level=major
   elif [[ $commit_messages_since_last_version == *"#minor"* ]]; then
     version_bump_level=minor
   fi
 
-  echo "version_bump_level:${version_bump_level}"
+  echo "Performing a ${version_bump_level} bump on the previouss version: ${previous_semantic_version}"
 
+  # Bump previous version with bump level
   new_version=$(semver bump "${version_bump_level}" "${previous_semantic_version}")
-  echo "new_version:${new_version}"
 fi
 
+# Add prefix to new version
 new_version_tag=${version_tag_prefix}${new_version}
-echo "new_version_tag:${new_version_tag}"
 
+# Create annotated tag and apply to the current commit
 git_tag_result=$(git tag -a ${new_version_tag} -m "Bump ${branch_name} to ${new_version}")
 echo "git_tag_result: ${git_tag_result}"
 git_tag_status=$?
 echo "git_tag_status: ${git_tag_status}"
 
+# Output new tag for use in other Github Action jobs
+echo "::set-output name=new_version_tag::${new_version_tag}"
 git_push_result=$(git push --tags)
 echo "git_push_result: ${git_push_result}"
 git_push_status=$?
