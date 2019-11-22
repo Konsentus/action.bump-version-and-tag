fd450c21b2236c8d26c8385999e5f487501d6040
odellt <actions@github.com>
2019-11-22 10:30:30 +0000

    :bookmark: Working with error handling and logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,56 @@
 #!/bin/bash -l
 
+die() {
+	local _ret=$2
+	test -n "$_ret" || _ret=1
+	printf "$1\n" >&2
+	exit ${_ret}
+}
+
+get_previous_version_tag() {
+  local tag_prefix=$1
+  local previous_version_tag
+  # Get last annoted tag that has the version tag prefix
+  previous_version_tag=$(git describe --abbrev=0 --match="${version_tag_prefix}*" 2>&1)
+  local git_decribe_exit_code=$?
+  if [ ${git_decribe_exit_code} -ne 0 ]; then
+    if [[ ${previous_version_tag} == *"No names found"* ]]; then
+      # Effectively an empty response so return success
+      return 0
+    else
+      return ${git_decribe_exit_code}
+    fi
+  fi
+  echo ${previous_version_tag}
+}
+
+get_bump_level_from_git_commit_messages() {
+  local bump_level="patch"
+  local previous_tag=$1
+  if [ -z ${previous_tag} ]; then
+    # Return default if no previous tag has been passed
+    echo ${bump_level}
+  fi
+  # Default version bump level is patch
+  local commit_messages
+  # Get all commit messages since the previous_tag
+  commit_messages=$(git log --pretty=oneline HEAD...${previous_tag})
+  local git_log_exit_code=$?
+  if [ ${git_log_exit_code} -ne 0 ]; then
+    return ${git_log_exit_code}
+  fi
+
+  # Search commit messages for presence of #major or #minor to determine verison bump level
+  # #major takes precedence
+  if [[ $commit_messages == *"#major"* ]]; then
+    bump_level="major"
+  elif [[ $commit_messages == *"#minor"* ]]; then
+    bump_level="minor"
+  fi
+
+  echo ${bump_level}
+}
+
 # Configure git cli tool
 git config --global user.email "actions@github.com"
 git config --global user.name "${GITHUB_ACTOR}"
@@ -10,53 +61,39 @@ branch_name=${GITHUB_REF##*/}
 # Create tag prefix
 version_tag_prefix=${branch_name}/v
 
-# Get last annotated tag from previous commits, matching the tag prefix pattern
-previous_version_tag=$(git describe --abbrev=0 --match="${version_tag_prefix}*")
+# Retrieve previous tag that matches the version tag prefix
+previous_version_tag=$(get_previous_version_tag ${version_tag_prefix}) || die "Failed to retrieve previous tags"
 
-if [ $? -ne 0 ] || [ -z "${previous_version_tag}" ]; then
-  # No previous tag found, so start version at 0.0.0
-  echo "Failed to find any previous version tags with the prefix ${version_tag_prefix}. Initial version will be 0.0.0"
-  new_version=0.0.0
+if [ -z ${previous_version_tag} ]; then
+  # No previous version tag found. Setting previous version to be 0.0.0
+  previous_version="0.0.0"
 else
+  # A previous tag matching the tag prefix was found, output it for future steps
   echo "::set-output name=previous_version_tag::${previous_version_tag}"
-
-  # Strip prefix from the previous version tag
-  previous_semantic_version=${previous_version_tag#${version_tag_prefix}}
-
-  # Gather commit message from all commits since the last version tag to now
-  commit_messages_since_last_version=$(git log --pretty=oneline HEAD...${previous_version_tag})
-
-  # Default version bump level is patch
-  version_bump_level=patch
-
-  # Search commit messages for presence of #major or #minor to determine verison bump level
-  # #major takes precedence
-  if [[ $commit_messages_since_last_version == *"#major"* ]]; then
-    version_bump_level=major
-  elif [[ $commit_messages_since_last_version == *"#minor"* ]]; then
-    version_bump_level=minor
-  fi
-
-  echo "Performing a ${version_bump_level} bump on the previouss version: ${previous_semantic_version}"
-
-  # Bump previous version with bump level
-  new_version=$(semver bump "${version_bump_level}" "${previous_semantic_version}")
+  previous_version=${previous_version_tag#${version_tag_prefix}}
 fi
+echo "Previous version tag: ${previous_version_tag:-"Not found"}"
+echo "Previous version: ${previous_version}"
 
-# Add prefix to new version
+# Get version bump level from previous commit messages
+bump_level=$(get_bump_level_from_git_commit_messages ${previous_version_tag}) || die "Failed to retrieve commit messages since previous tag"
+echo "Version bump level: ${bump_level}"
+
+# Bump the version number
+new_version=$(semver bump ${bump_level} ${previous_version}) || die "Failed to bump the ${bump_level} version of ${previous_version}"
+
+# Add prefix to new version to create the new tag
 new_version_tag=${version_tag_prefix}${new_version}
 
 # Create annotated tag and apply to the current commit
-git_tag_result=$(git tag -a ${new_version_tag} -m "Bump ${branch_name} to ${new_version}")
-echo "git_tag_result: ${git_tag_result}"
-git_tag_status=$?
-echo "git_tag_status: ${git_tag_status}"
+git_tag_result=$(git tag -a ${new_version_tag} -m "Bump ${branch_name} to ${new_version}") || die "Failed to apply tag: ${new_version_tag}"
 
 # Output new tag for use in other Github Action jobs
+git_push_result=$(git push --tags) || die "Failed to push new tag"
+
+# Output the new tag for future jobs
 echo "::set-output name=new_version_tag::${new_version_tag}"
-git_push_result=$(git push --tags)
-echo "git_push_result: ${git_push_result}"
-git_push_status=$?
-echo "git_push_status: ${git_push_status}"
-echo "FINISHED"
+
+echo "Commit SHA: ${GITHUB_SHA} has been tagged with ${new_version_tag}"
+echo "Successfully performed ${GITHUB_ACTION}"
 # exit with a non-zero status to flag an error/failure
