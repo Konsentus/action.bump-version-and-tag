2317c3d61c10032ca65c4a4f6fd1a2f71f186e3c
odellt <actions@github.com>
2019-11-25 16:54:09 +0000

    :sparkles: Adds handling for hotfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -l
 
+# Convenience function to output an error message and exit with non-zero error code
 die() {
 	local _ret=$2
 	test -n "$_ret" || _ret=1
@@ -18,6 +19,7 @@ get_previous_version_tag() {
       # Effectively an empty response so return success
       return 0
     else
+      echo ${previous_version_tag} >&2
       return ${git_decribe_exit_code}
     fi
   fi
@@ -52,6 +54,34 @@ get_bump_level_from_git_commit_messages() {
   echo ${bump_level}
 }
 
+check_is_hotfix() {
+  local branch_list
+  branch_list=$(git branch --contains="${GITHUB_SHA}") || return 1
+  local number_of_branches=$(wc -l <<< ${branch_list}) || return 1
+  number_of_branches=$(echo -n "${number_of_branches//[[:space:]]/}")
+  if [[ number_of_branches -eq "2" ]] && [[ ${branch_list}==*"hotfix/"* ]] && [[ ${branch_list}==*"${branch_name}"* ]]; then
+    echo true
+    return 0
+  fi
+  echo false
+  return 0
+}
+
+move_previous_tag() {
+  local previous_version_tag=$1
+  local current_commit_sha=$2
+  git tag -a -m "Hotfix applied" "${previous_version_tag}" "${current_commit_sha}" -f
+  if [ $? -ne 0 ]; then
+    echo "Failed to move tag for hotfix" >&2
+    return 1
+  fi
+  git push origin --tags -f
+  if [ $? -ne 0 ]; then
+    echo "Failed to push hotfix tag" >&2
+    return 1
+  fi
+}
+
 # Configure git cli tool
 git config --global user.email "actions@github.com"
 git config --global user.name "${GITHUB_ACTOR}"
@@ -77,27 +107,34 @@ fi
 echo "Previous version tag: ${previous_version_tag:-"Not found"}"
 echo "Previous version: ${previous_version}"
 
-# Get version bump level from previous commit messages
-bump_level=$(get_bump_level_from_git_commit_messages ${previous_version_tag}) || die "Failed to retrieve commit messages since previous tag"
-echo "Version bump level: ${bump_level}"
+is_hotfix=$(check_is_hotfix) || die "failed to check if hotfix"
+echo "is hotfix: ${is_hotfix}"
 
-# Bump the version number
-new_version=$(semver bump ${bump_level} ${previous_version}) || die "Failed to bump the ${bump_level} version of ${previous_version}"
+if [ "${is_hotfix}" == false ]; then
+    # Get version bump level from previous commit messages
+    bump_level=$(get_bump_level_from_git_commit_messages ${previous_version_tag}) || die "Failed to retrieve commit messages since previous tag"
+    echo "Version bump level: ${bump_level}"
 
+    # Bump the version number
+    new_version=$(semver bump ${bump_level} ${previous_version}) || die "Failed to bump the ${bump_level} version of ${previous_version}"
+    new_version_tag=${version_tag_prefix}${new_version}
+    tag_message="Bump ${branch_name} to ${new_version}"
+else
+  # move_previous_tag "${previous_version_tag}" "${GITHUB_SHA}" || die "Failed to move tag for hotfix"
+  new_version_tag=${previous_version_tag}
+  tag_message="Apply hotfix"
+fi
 # Add prefix to new version to create the new tag
-new_version_tag=${version_tag_prefix}${new_version}
 
 echo "Tagging latest ${branch_name} with ${new_version_tag}"
 # Create annotated tag and apply to the current commit
-git_tag_result=$(git tag -a ${new_version_tag} -m "Bump ${branch_name} to ${new_version}") || die "Failed to apply tag: ${new_version_tag}"
+git tag -a -m "Hotfix applied" "${new_version_tag}" "${GITHUB_SHA}" -f || die "Failed to ${tag_message}"
 
 echo "Pushing tags"
+git push origin --tags -f || die "Failed to push ${tag_message}"
+
 # Output new tag for use in other Github Action jobs
-git_push_result=$(git push "${remote_repo}" --tags) || die "Failed to push new tag"
-
-# Output the new tag for future jobs
 echo "::set-output name=new_version_tag::${new_version_tag}"
-
 echo "Commit SHA: ${GITHUB_SHA} has been tagged with ${new_version_tag}"
 echo "Successfully performed ${GITHUB_ACTION}"
 # exit with a non-zero status to flag an error/failure
