d5ae786aeb42bc0e0f4c20c55afb13171a73657f
odellt <actions@github.com>
2019-11-26 16:21:10 +0000

    :loud_sound: Add extra logs for debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,34 +11,43 @@ die() {
 get_previous_version_tag() {
   local tag_prefix=$1
   local previous_version_tag
+
   # Get last annoted tag that has the version tag prefix
+  # stderr redirected to stdout to collect any error messages
   previous_version_tag=$(git describe --abbrev=0 --match="${version_tag_prefix}*" 2>&1)
   local git_decribe_exit_code=$?
+
   if [ ${git_decribe_exit_code} -ne 0 ]; then
     if [[ ${previous_version_tag} == *"No names found"* ]]; then
       # Effectively an empty response so return success
       return 0
     else
+      # Something else went wrong, print error to stderr and exit with non zero
       echo ${previous_version_tag} >&2
       return ${git_decribe_exit_code}
     fi
   fi
+
+  # everything went fine and the previous version_tag was found
   echo ${previous_version_tag}
 }
 
 get_bump_level_from_git_commit_messages() {
-  local bump_level="patch"
   local previous_tag=$1
+  local bump_level="patch"
+  local commit_messages
+
   if [ -z ${previous_tag} ]; then
     # Return default if no previous tag has been passed
     echo ${bump_level}
     return 0
   fi
+
   # Default version bump level is patch
-  local commit_messages
-  # Get all commit messages since the previous_tag
+  # Get all commit titles since the previous_tag
   commit_messages=$(git log --pretty=oneline HEAD...${previous_tag})
   local git_log_exit_code=$?
+
   if [ ${git_log_exit_code} -ne 0 ]; then
     return ${git_log_exit_code}
   fi
@@ -55,22 +64,26 @@ get_bump_level_from_git_commit_messages() {
 }
 
 check_is_hotfix() {
-  echo "github SHA: ${GITHUB_SHA}" >&2
   local previous_commit
-  previous_commit=$(git rev-parse "${GITHUB_SHA}"^1) || return 1
-  echo "previous_commit: ${previous_commit}" >&2
   local branch_list
+  local number_of_branches
+
+  # get ancestor commit.
+  previous_commit=$(git rev-parse "${GITHUB_SHA}"^1) || return 1
+  # get list of branches (from remote) that contain the ancestor commit
   branch_list=$(git branch --contains="${previous_commit}" -r) || return 1
-  echo "branch name: ${branch_name}" >&2
-  echo "branch list: ${branch_list}" >&2
-  local number_of_branches=$(wc -l <<< "${branch_list}") || return 1
-  echo "number of branches: ${number_of_branches}" >&2
+  # count new lines to get number of branches
+  number_of_branches=$(wc -l <<< "${branch_list}") || return 1
+  # trim whitespace returned from wc command
   number_of_branches=$(echo -n "${number_of_branches//[[:space:]]/}")
-  echo "number of branches after trim: ${number_of_branches}" >&2
+
+  # if there are exactly two branches, one of which is a "hotfix" branch and the other is the current branch
+  # then this is considered a hotfix
   if [[ number_of_branches -eq "2" ]] && [[ ${branch_list}==*"hotfix/"* ]] && [[ ${branch_list}==*"${branch_name}"* ]]; then
     echo true
     return 0
   fi
+
   echo false
   return 0
 }
@@ -78,11 +91,14 @@ check_is_hotfix() {
 move_previous_tag() {
   local previous_version_tag=$1
   local current_commit_sha=$2
+
+  # force reuse of previous tag, will remove it from its original commit
   git tag -a -m "Hotfix applied" "${previous_version_tag}" "${current_commit_sha}" -f
   if [ $? -ne 0 ]; then
     echo "Failed to move tag for hotfix" >&2
     return 1
   fi
+  # force push of moved tag
   git push origin --tags -f
   if [ $? -ne 0 ]; then
     echo "Failed to push hotfix tag" >&2
@@ -113,10 +129,9 @@ else
   previous_version=${previous_version_tag#${version_tag_prefix}}
 fi
 echo "Previous version tag: ${previous_version_tag:-"Not found"}"
-echo "Previous version: ${previous_version}"
 
+# Perform check do determine if this was triggered by a hotfix
 is_hotfix=$(check_is_hotfix) || die "failed to check if hotfix"
-echo "is hotfix: ${is_hotfix}"
 
 if [ "${is_hotfix}" == false ]; then
     # Get version bump level from previous commit messages
@@ -126,17 +141,18 @@ if [ "${is_hotfix}" == false ]; then
     # Bump the version number
     new_version=$(semver bump ${bump_level} ${previous_version}) || die "Failed to bump the ${bump_level} version of ${previous_version}"
     new_version_tag=${version_tag_prefix}${new_version}
-    tag_message="Bump ${branch_name} to ${new_version}"
+    tag_message="Bump ${branch_name} tag from ${previous_version_tag} to ${new_version_tag}"
 else
+  echo "Hotfix detected. Moving previous version tag to current latest on ${branch_name}"
   # move_previous_tag "${previous_version_tag}" "${GITHUB_SHA}" || die "Failed to move tag for hotfix"
   new_version_tag=${previous_version_tag}
-  tag_message="Apply hotfix"
+  tag_message="Apply hotfix, moving ${previous_version_tag} to latest ${branch_name}"
 fi
 # Add prefix to new version to create the new tag
 
 echo "Tagging latest ${branch_name} with ${new_version_tag}"
 # Create annotated tag and apply to the current commit
-git tag -a -m "Hotfix applied" "${new_version_tag}" "${GITHUB_SHA}" -f || die "Failed to ${tag_message}"
+git tag -a -m "${tag_message}" "${new_version_tag}" "${GITHUB_SHA}" -f || die "Failed to ${tag_message}"
 
 echo "Pushing tags"
 git push "${remote_repo}" --tags -f || die "Failed to push ${tag_message}"
